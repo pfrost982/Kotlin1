22b0048cefc40144e1eb71aae83522ef52c37c3b
pfrost982 <power982@gmail.com>
2021-07-04 22:06:28 +0700

    Initial commit

---
+++ app/src/main/java/ru/gb/kotlin1/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.gb.kotlin1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
